1e159be5aacb739148656e1e4be94fffc252128a
chet <schreiberbrett@gmail.com>
2023-01-26 19:41:31 -0500

    Embedding the code directly

--- code.scm
+++ code.scm
@@ -1,1 +1,11 @@
-(define x 123)
+(defrel (appendo l r o)
+    (conde
+        ((== l '()) (== r o))
+
+        ((fresh (h tl to)
+            (== l `(,h . ,tl))
+            (== o `(,h . ,to))
+            (appendo tl r to)))))
+
+(defrel (riffleo x y z)
+    
