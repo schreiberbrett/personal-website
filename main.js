a285a2cce3e1f008a0523051d93050da16f074e2
chet <schreiberbrett@gmail.com>
2023-01-26 19:06:45 -0500

    Load source code

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+// @ts-check
+
+var x = new XMLHttpRequest();
+var sourceCode = new URL('https://raw.githubusercontent.com/schreiberbrett/personal-website/master/code.scm')
+
+fetch(sourceCode).then(response => {
+    if (response.ok) {
+        response.text().then(x => {
+            console.log('I found:', x)
+        })
+    }
+});
+
+
 console.log('Hello, world!');
